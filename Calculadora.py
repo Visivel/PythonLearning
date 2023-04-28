bb88073e774fa0574a451dffec7799810bb44567
Visivel <120154724+Visivel@users.noreply.github.com>
2023-04-28 17:10:30 -0400

    Minha REAL primeira calculadora
    Real calculadora pq eu tava fazendo o bgl da tabuada ai eu pensei, em fazer uma tabuada em que voce pode escolher ate onde ela vai fazer a tabuada, mas ai eu repensei e acabei que fazendo logo uma calculadora kkkk, mas minha cabeca acabou que bugando tudo e acabei falando que a tabuada era minha primeira calculadora ou algo assim.
    
    ESSA É A MINHA REAL PRIMEIRA CALCULADORA

---
+++ Calculadora.py
@@ -0,0 +1,16 @@
+num = int(input("Qual numero voce deseja calcular?\n>>> "))
+metodo = int(input("Qual metodo?\n(1) Soma\n(2) Subtracao\n(3) Divisao\n(4) Multiplicação\n(Use 1,2,3,4 para responder)\n>>> "))
+calc = int(input("Qual numero vai usar para calcular?\n(Qual vai mulplicar, somar etc)\n>>> "))
+if metodo==1:
+    print("A soma de {} com {} é:\n[{}]".format(num,calc,num+calc))
+else:
+    if metodo==2:
+        print("A subtracao de {} com {} é:\n[{}]".format(num,calc,num-calc))
+    else:
+        if metodo==3:
+            print("A divisao de {} com {} é:\n[{}]".format(num,calc,round(num/calc,3)))
+            # Use round pq acho que nos outros so vai Float caso for proposital
+            # Tipo caso a pessoa coloque 0.444 no num, ai ele vai ficar locao msm.
+        else:
+            if metodo==4:
+                print("A multiplicação de {} com {} é:\n[{}]".format(num,calc,num*calc))
