299ab2db4bf5bdbdb5cf6bed9a6886840b7506a0
Visivel <120154724+Visivel@users.noreply.github.com>
2023-04-28 15:04:29 -0400

    Minha primeira "calculadora"
    Acho que essa é a minha primeira "Calculadora", ta mais pra calculadora de tabuada mas ehh, bem legal.

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+n = int(input("Qual numero voce deseja ver ele na tabuada?\n>>> "))
+print("+----A tabuada do {}----+\n"
+      # Favor, ignorar meu codigo lindo e organizado, muito lindo
+      # Quis deixar ele mais "organizado" assim e legivel, mas dava pra deixar mais compactado
+      "{} x 0 = {}\n"
+      "{} x 1 = {}\n"
+      "{} x 2 = {}\n"
+      "{} x 3 = {}\n"
+      "{} x 4 = {}\n"
+      "{} x 5 = {}\n"
+      "{} x 6 = {}\n"
+      "{} x 7 = {}\n"
+      "{} x 8 = {}\n"
+      "{} x 9 = {}\n"
+      "{} x 10 = {}\n"
+      "+----------------------+"
+      # Parte matematica e ferrada
+      .format(n,n,n*0,n,n*1,n,n*2,n,n*3,n,n*4,n,n*5,n,n*6,n,n*7,n,n*8,n,n*9,n,n*10))
