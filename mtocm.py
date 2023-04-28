3da950cc0e0de4bd27b789e76844b98c0294be8b
Visivel <120154724+Visivel@users.noreply.github.com>
2023-04-28 14:51:05 -0400

    Create mtocm.py
    FUcking \n, shit code mf fr fr

---
+++ mtocm.py
@@ -0,0 +1,2 @@
+metro = int(input("Quantos metros dejesa converter?\n(O valor sera convertido em Centrimetros e Milimetros)\n>>> "))
+print("{} metros convertidos serao {} centimentros\nJa em milimetros, serao {}".format(metro,metro*100,metro*1000))
