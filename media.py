aa0c17d7237a897fd401f93806f786eb4c486fed
Visivel <120154724+Visivel@users.noreply.github.com>
2023-05-08 14:26:15 -0400

    Create media.py

---
+++ media.py
@@ -0,0 +1,4 @@
+nota = int(input("Qual sua nota no primeiro bimestre?\n>>> "))
+nota2 = int(input("Qual sua nota no segundo bimestre?\n>>> "))
+print(int(2/nota+nota2))
+# Nem sei se essa porra ta certa kkkkkkkk
