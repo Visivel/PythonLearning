f5c1aeb871a53a32efc548664ec3e49f60242bd7
Visivel <120154724+Visivel@users.noreply.github.com>
2023-05-08 16:04:55 -0400

    Visual melhorado
    sei la pra que voce iria usar isso, eu prefiro o antigo

--- TabuadaRewrite.py
+++ TabuadaRewrite.py
@@ -2,5 +2,5 @@ n = int(input("Qual numero voce deseja ver ele na tabuada?\n>>> "))
 n2 = int(input("Ate onde voce quer que a tabuada va?\n>>> "))
 print("+----A tabuada do {}----+".format(n))
 for calc in range(n2+1):
-    print(">> {} x {} = {}".format(n,calc,n*calc))
+    print(">> {} x {:2} = {}".format(n,calc,n*calc))
 print("+{}+".format("-"*22))
