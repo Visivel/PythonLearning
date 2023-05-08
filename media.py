6b8d16b809629c7f18175cb3022ad4c4b494878c
Visivel <120154724+Visivel@users.noreply.github.com>
2023-05-08 14:44:43 -0400

    I AM A FUCKING MONKEY I CANT TYPE

--- media.py
+++ media.py
@@ -1,4 +1,4 @@
 nota = int(input("Qual sua nota no primeiro bimestre?\n>>> "))
 nota2 = int(input("Qual sua nota no segundo bimestre?\n>>> "))
-print(int(2/nota+nota2))
+print(int("A media do primeiro e segundo bimestre é {}".format(2/nota+nota2)))
 # Nem sei se essa porra ta certa kkkkkkkk
