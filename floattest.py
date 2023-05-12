54bc49948910f6f82b23d7a4d1478aa0e53ca334
Visivel <120154724+Visivel@users.noreply.github.com>
2023-05-12 15:50:32 -0400

    Create floattest.py
    é a mesma coisa

---
+++ floattest.py
@@ -0,0 +1,4 @@
+from math import trunc
+x = float(input("Teste de math e int usando numero float.\nDigite um numero float...\n>>> "))
+print("Com math: {}".format(trunc(x)))
+print("Sem math: {}".format((int(x))))
